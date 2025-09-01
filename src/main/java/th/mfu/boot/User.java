adc677e2e8edd1c78cae9df0e672a6653bfe7328
Ratthaphum  Wanthamat <6731503031@lamduan.mfu.ac.th>
2025-09-01 09:45:54 +0000

    DONE

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -1,6 +1,54 @@
 package th.mfu.boot;
 
-public class User {
+import java.time.LocalDate;
 
+import org.springframework.lang.NonNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
+public class User {
+    private String username;
+    private String address;
+    private String email;
+    private String phone;
+    private LocalDate birthday;
+    
+    public String getUsername() {
+    return username;
+    }
+
+    public void setUsername(String username) {
+    this.username = username;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
     
 }
--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -6,31 +6,41 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    public static Map<String, User> users = new HashMap<String, User>();
+    private static Map<String, User> users = new HashMap<>();
 
-   
-    public ResponseEntity<String> registerUser( User user) {
-      //TODO
-      return null;
+    @PostMapping("/users")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        if(users.containsKey(user.getUsername())) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
+        }
+        users.put(user.getUsername(), user);
+        return new ResponseEntity<>("User created", HttpStatus.CREATED);
     }
 
+    @GetMapping("/users/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User user = users.get(username);
+        if(user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
     public ResponseEntity<Collection<User>> list() {
-        //TODO
-        return null;
+        return new ResponseEntity<>(users.values(), HttpStatus.OK);
     }
 
-    public ResponseEntity<User> getUser(String username) {
-        //TODO
-        return null;
+    @DeleteMapping("/users/{username}")
+    public ResponseEntity<String> deleteUser(@PathVariable String username) {
+        if (users.remove(username) == null) {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>("User deleted", HttpStatus.NO_CONTENT);
     }
-    
 }
